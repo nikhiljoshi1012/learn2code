ffa6d177106caf85e9ff382ea9eb58cab0d11f5e
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-07-31 22:33:30 +0530

    fix: gh-pages

--- learn2code/next.config.mjs
+++ learn2code/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/learn2code",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
