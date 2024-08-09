769650a124a086947bb0d7f4838ae20bfde17197
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-08-09 23:00:48 +0530

    fix: deploy asset management

--- learn2code/next.config.mjs
+++ learn2code/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: '/learn2code',
+    assetPrefix: '/learn2code',
     output: 'export',
     distDir: 'out',
     reactStrictMode: true,
