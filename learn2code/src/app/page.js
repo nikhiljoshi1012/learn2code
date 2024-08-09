8f6af57caf3f8e7a1c6fd3eb7a49faf2d6a1451e
Pratyush Landekar <pratyushlandekar103@gmail.com>
2024-08-09 23:23:43 +0530

    chore: deployment asset fix

--- learn2code/src/app/page.js
+++ learn2code/src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  // const router = useRouter();
+  const basePath = "/learn2code"; //TODO: This is an non-ideal way for asset prefix 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -17,7 +19,7 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
+              src={`${basePath}/vercel.svg`}
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +33,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${basePath}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
