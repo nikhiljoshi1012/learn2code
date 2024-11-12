b29276f030881b573a4f774218b021d1e48e4d7e
Pratyush <pratyushlandekar103@gmail.com>
2024-11-12 11:54:17 +0530

    feat: close to dozen blogs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  devToolbar: { enabled: false, },
   integrations: [react()],
   output: 'server',
   adapter: node({
